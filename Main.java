82739e7ca5bb9449242faf93f62108fe32bc6959
Abhishek Abhishek <abhiuee@synopsys.com>
2017-12-01 15:17:26 -0700

    Change main things

---
+++ Main.java
@@ -0,0 +1,14 @@
+/**
+ * This file holds the main code that simplifies running the challenge code.
+ *
+ */
+public class Main
+{
+    private Main() {
+    }
+
+    public static void main(String[] args)
+    {
+        new RadonClientApplication().run();
+    }
+}
